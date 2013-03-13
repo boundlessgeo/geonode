f8de5fd7fb40868963c3a0ca15dbf263995a013e
Ivan Willig <iwillig@gmail.com>
2013-03-13 11:46:08 -0400

    Move some of the toggle size methods to the view class, create an event that is fired when the state is changed

--- src/geonode-client/app/static/script/app/GeonodeViewer.js
+++ src/geonode-client/app/static/script/app/GeonodeViewer.js
@@ -102,7 +102,13 @@ var GeonodeViewer = Ext.extend(gxp.Viewer, {
      *  ``RegExp``
      */
     urlPortRegEx: /^(http[s]?:\/\/[^:]*)(:80|:443)?\//,
-    
+
+    /** private: property[fullScreen]
+     *  Boolean
+     */
+    fullScreen: false,
+
+
     //public variables for string literals needed for localization
     backgroundContainerText: "UT:Background",
     connErrorTitleText: "UT:Connection Error",
@@ -135,7 +141,8 @@ var GeonodeViewer = Ext.extend(gxp.Viewer, {
              * Fires before the page unloads. Return false to stop the page
              * from unloading.
              */
-            "beforeunload"
+            "beforeunload",
+            "toggleSize"
         );
         
         //Add special connection handlers
@@ -143,7 +150,11 @@ var GeonodeViewer = Ext.extend(gxp.Viewer, {
         
         //Globally register the color manager for any field
         this.registerColorManager();
-        
+
+        this.on('portalready', function () {
+            this.toggleMapSize();
+        });
+
         // limit combo boxes to the window they belong to - fixes issues with
         // list shadow covering list items
         Ext.form.ComboBox.prototype.getListParent = function(){
@@ -156,7 +167,43 @@ var GeonodeViewer = Ext.extend(gxp.Viewer, {
         
         GeonodeViewer.superclass.constructor.apply(this, [config]);
     },
-    
+
+    toggleMapSize: function () {
+        // we have access to the mapPanel and the portal, both of
+        // which we need
+        var hash = window.location.hash;
+        if (hash === '#full') {
+            this.setMaxMapSize();
+        } else {
+            this.setMinMapSize();
+        }
+    },
+
+    setMaxMapSize: function () {
+        var headerHeight =
+            Ext.get('header').getHeight() +
+            Ext.get('top-crossbar').getHeight() +
+            Ext.get('crossbar').getHeight(),
+            main = Ext.get('main'),
+            newWidth = window.innerWidth + 1, // why the +1 ?
+            newHeight = window.innerHeight - headerHeight + 2; // why ?
+
+        this.portal.setSize(newWidth, newHeight);
+        this.portal.el.alignTo(main, 'tl-tl', [-8, 0]);
+        Ext.getBody().setStyle({overflow: 'hidden'});
+        window.scrollTo(0, 0);
+
+        this.fullScreen = true;
+        this.fireEvent('toggleSize', this.fullScreen);
+    },
+
+    setMinMapSize: function () {
+        // do some stuff to set the screen to the smallest size
+        this.fullScreen = false;
+        this.fireEvent('toggleSize', this.fullScreen);
+
+    },
+
     loadConfig: function(config, callback){
         var hasConfig = config && (config.tools || config.sources || config.map);
         config = Ext.apply(config ||
