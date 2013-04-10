06dc930ef8a405b384ab98fb2d38d97351fc57be
Ivan Willig <iwillig@gmail.com>
2013-04-10 12:18:29 -0400

    Merge a lot of the map props widget functionality into the save map widget

--- src/geonode-client/app/static/script/app/mapstory/ToolBar.js
+++ src/geonode-client/app/static/script/app/mapstory/ToolBar.js
@@ -10,6 +10,82 @@
         ptype: 'ms-tool-bar',
         saveText: 'UT: Save Map',
         publishText: 'UT: Publish Map',
+        zoomText: 'UT: Number of zoom levels',
+
+        buildSaveForm: function () {
+            var ge = this.target,
+                updateMap = function (asCopy) {
+                    var saveForm = Ext.getCmp('saveForm').getForm(),
+                        values = saveForm.getValues();
+                    this.about.title = values.mapTitle;
+                    this.about.abstract = values.mapAbstract;
+                    if (asCopy) {
+                        this.save(true);
+                    } else { this.save(); }
+                    this.metadataForm.hide();
+                },
+                saveButton = new Ext.Button({
+                    text: ge.metadataFormSaveText,
+                    handler: function (e) {
+                        updateMap.call(this, false);
+                    },
+                    scope: ge
+                }),
+                saveAsButton = new Ext.Button({
+                    text: ge.metadataFormSaveAsCopyText,
+                    handler: function (e) {
+                        updateMap.call(this, true);
+                    },
+                    scope: ge
+                }),
+                saveWindow = new Ext.Window({
+                    title: ge.metaDataHeader,
+                    width: 400, // if we don't set a min value chrome miss
+                    // rendered the window
+                    items: [
+                        {
+                            xtype: 'form',
+                            labelAlign: 'top',
+                            bodyStyle: {
+                                padding: '5px'
+                            },
+                            id: 'saveForm',
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    id: 'mapTitle',
+                                    width: '95%',
+                                    fieldLabel: ge.metaDataMapTitle,
+                                    allowBlank: false,
+                                    value: ge.about.title
+                                },
+                                {
+                                    xtype: 'textarea',
+                                    id: 'mapAbstract',
+                                    width: '95%',
+                                    height: 200,
+                                    fieldLabel: ge.metaDataMapAbstract,
+                                    value: ge.about.abstract
+                                },
+                                {
+                                    xtype: 'numberfield',
+                                    allowNegative: false,
+                                    allowDecimals: false,
+                                    fieldLabel: this.zoomText
+                                }
+                            ]
+                        }
+                    ],
+                    bbar: [
+                        "->",
+                        saveButton,
+                        saveAsButton
+                    ]
+                });
+
+            return saveWindow;
+        },
+
         addOutput: function () {
             return mapstory.ToolBar.superclass.addOutput.call(this, {
                 xtype: 'toolbar',
@@ -19,9 +95,12 @@
                         text: this.saveText,
                         scope: this,
                         handler: function () {
-                            this.target.showMetadataForm();
+                            if (!this.target.metadataForm) {
+                                this.target.metadataForm = this.buildSaveForm();
+                            }
+                            this.target.metadataForm.show();
                         }
-                    },
+                    }
                 ]
             });
         }
