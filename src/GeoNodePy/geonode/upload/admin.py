fb996c2abf268645013c6b1da73c74ab157b0011
Ian Schneider <planablediglet@gmail.com>
2013-06-28 13:51:00 -0600

    better info in Upload admin on uploadsession state

--- src/GeoNodePy/geonode/upload/admin.py
+++ src/GeoNodePy/geonode/upload/admin.py
@@ -1,6 +1,8 @@
 from geonode.upload.models import Upload
 
+from django import forms
 from django.contrib import admin
+import pprint
 
 
 def import_link(obj):
@@ -8,9 +10,19 @@ def import_link(obj):
 import_link.short_description = 'Link'
 import_link.allow_tags = True
 
+
+class UploadAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kw):
+        super(UploadAdminForm, self).__init__(*args, **kw)
+        session = self.instance.get_session()
+        print session
+        if session:
+            self.initial['session'] = pprint.pformat(vars(session))
+
 class UploadAdmin(admin.ModelAdmin):
     list_display = ('user','date', 'state', import_link)
     date_hierarchy = 'date'
     list_filter = ('user','state')
+    form = UploadAdminForm
     
 admin.site.register(Upload, UploadAdmin)
