2b0c28cfa31fda7e31deab712aebdfeaf4bf9941
Bart van den Eijnden <bartvde@opengeo.org>
2013-09-04 04:57:18 -0700

    correction for timezone is now done at the gxp level, so don't do it more than once

--- src/geonode-client/app/static/script/app/mapstory/Notes.js
+++ src/geonode-client/app/static/script/app/mapstory/Notes.js
@@ -200,9 +200,7 @@ mapstory.plugins.NotesManager = Ext.extend(gxp.plugins.Tool, {
         var me = this;
         // override to get date time picker to use current time in playback
         gxp.form.ExtendedDateField.prototype.getPickerDate = function() {
-            var d = new Date(me.playback.playbackToolbar.control.currentValue);
-            d.setTime( d.getTime() + d.getTimezoneOffset()*60*1000 );
-            return d;
+            return new Date(me.playback.playbackToolbar.control.currentValue);
         };
         var output = mapstory.plugins.NotesManager.superclass.addOutput.call(this, {
             xtype: 'gxp_featuregrid',
@@ -432,15 +430,11 @@ mapstory.plugins.NotesManager = Ext.extend(gxp.plugins.Tool, {
                 'in_map': {xtype: 'checkbox'},
                 'in_timeline': {xtype: 'checkbox'},
                 'start_time': {id: 'start-time', xtype: 'gxp_datetimefield', todayText: 'Now', selectToday: function() {
-                    var d = new Date(me.playback.playbackToolbar.control.currentValue);
-                    d.setTime( d.getTime() + d.getTimezoneOffset()*60*1000 );
-                    this.setValue(d);
+                    this.setValue(new Date(me.playback.playbackToolbar.control.currentValue));
                     this.fireEvent('select', this, this.value);
                 }},
                 'end_time': {id: 'end-time', xtype: 'gxp_datetimefield', todayText: 'Now', selectToday: function() {
-                    var d = new Date(me.playback.playbackToolbar.control.currentValue);
-                    d.setTime( d.getTime() + d.getTimezoneOffset()*60*1000 );
-                    this.setValue(d);
+                    this.setValue(new Date(me.playback.playbackToolbar.control.currentValue));
                     this.fireEvent('select', this, this.value);
                 }},
                 'content': {xtype: 'textarea'},
