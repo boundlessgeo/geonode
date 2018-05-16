96fdbb87155e0500d0facc8841b0dce92a982c1c
Amiram Rahav <arahav@boundlessgeo.com>
2018-05-16 12:28:40 -0400

    import missing _user_has_perm

--- geonode/people/models.py
+++ geonode/people/models.py
@@ -21,7 +21,7 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
-from django.contrib.auth.models import AbstractUser, UserManager, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser, UserManager, AbstractBaseUser, _user_has_perm
 from django.db.models import signals
 from django.conf import settings
 
