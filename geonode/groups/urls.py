7738ee00e2baba31ffe014552e72a14e60ff1a43
Travis Brundage <travislbrundage@gmail.com>
2019-07-11 11:29:45 -0700

    Removing Group Activity page

--- geonode/groups/urls.py
+++ geonode/groups/urls.py
@@ -21,7 +21,7 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-from .views import GroupDetailView, GroupActivityView
+from .views import GroupDetailView
 
 urlpatterns = patterns('geonode.groups.views',
                        url(r'^$', TemplateView.as_view(template_name='groups/group_list.html'), name="group_list"),
@@ -37,5 +37,4 @@ urlpatterns = patterns('geonode.groups.views',
                        url(r'^group/(?P<slug>[-\w]+)/join/$', 'group_join', name='group_join'),
                        url(r'^group/[-\w]+/invite/(?P<token>[\w]{40})/$', 'group_invite_response',
                            name='group_invite_response'),
-                       url(r'^group/(?P<slug>[-\w]+)/activity/$', GroupActivityView.as_view(), name='group_activity'),
                        )
